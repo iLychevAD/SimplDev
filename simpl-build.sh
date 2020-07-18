2d233562305e44020a6dd77eff89a05d99aefacb
Ivan Lychev <47522411+iLychevAD@users.noreply.github.com>
2020-07-18 17:16:38 +0300

    Update simpl-build.sh

--- simpl-build.sh
+++ simpl-build.sh
@@ -16,8 +16,8 @@ echo "DOTNET BUILD CMD"
 dotnet restore && dotnet build
 
 cd src/SimplCommerce.WebHost \
-	&& dotnet ef migrations add initialSchema \
-	&& dotnet ef database update
+	&& dotnet ef migrations add initialSchema
+#&& dotnet ef database update
 	
 echo "The database schema has been created. Please execute the src/Database/StaticData_Postgres.sql to insert static data."
 echo "Then type 'dotnet run' in src/SimplCommerce.WebHost to start the app."
