6312d3c588da0530dc2c7737867c06a236e00d4c
Ivan Lychev <47522411+iLychevAD@users.noreply.github.com>
2020-07-18 16:52:44 +0300

    some debug

--- simpl-build.sh
+++ simpl-build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 echo "SEDding files"
-sed -i'' -e 's|<PackageReference Include="Microsoft.EntityFrameworkCore.SqlServer" Version="2.2.1" />|<PackageReference Include="Npgsql.EntityFrameworkCore.PostgreSQL" Version="2.2.0" />|' src/SimplCommerce.WebHost/SimplCommerce.WebHost.csproj
+sed -i -e 's|<PackageReference Include="Microsoft.EntityFrameworkCore.SqlServer" Version="2.2.1" />|<PackageReference Include="Npgsql.EntityFrameworkCore.PostgreSQL" Version="2.2.0" />|' src/SimplCommerce.WebHost/SimplCommerce.WebHost.csproj
 cat src/SimplCommerce.WebHost/SimplCommerce.WebHost.csproj
-sed -i'' -e 's/UseSqlServer/UseNpgsql/' src/SimplCommerce.WebHost/Program.cs
-sed -i'' -e 's/UseSqlServer/UseNpgsql/' src/SimplCommerce.WebHost/Extensions/ServiceCollectionExtensions.cs
+echo "GREP"
+grep EntityFrameworkCore src/SimplCommerce.WebHost/SimplCommerce.WebHost.csproj
+sed -i -e 's/UseSqlServer/UseNpgsql/' src/SimplCommerce.WebHost/Program.cs
+sed -i -e 's/UseSqlServer/UseNpgsql/' src/SimplCommerce.WebHost/Extensions/ServiceCollectionExtensions.cs
 
-sed -i'' -e "s/DB_CONNECTION_PLACEHOLDER/${DB_CONNECTION}/" src/SimplCommerce.WebHost/appsettings.json
+sed -i -e "s/DB_CONNECTION_PLACEHOLDER/${DB_CONNECTION}/" src/SimplCommerce.WebHost/appsettings.json
 echo "SED done"
 
 rm -rf src/SimplCommerce.WebHost/Migrations/*
