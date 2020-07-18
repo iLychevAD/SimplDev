ae49686948d53c1eb1eb8dd97de7a31a42f9959e
Ivan Lychev <47522411+iLychevAD@users.noreply.github.com>
2020-07-18 16:38:59 +0300

    Update simpl-build.sh

--- simpl-build.sh
+++ simpl-build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
-
+echo "SEDding files"
 sed -i'' -e 's|<PackageReference Include="Microsoft.EntityFrameworkCore.SqlServer" Version="2.2.1" />|<PackageReference Include="Npgsql.EntityFrameworkCore.PostgreSQL" Version="2.2.0" />|' src/SimplCommerce.WebHost/SimplCommerce.WebHost.csproj
 sed -i'' -e 's/UseSqlServer/UseNpgsql/' src/SimplCommerce.WebHost/Program.cs
 sed -i'' -e 's/UseSqlServer/UseNpgsql/' src/SimplCommerce.WebHost/Extensions/ServiceCollectionExtensions.cs
 
 sed -i'' -e "s/DB_CONNECTION_PLACEHOLDER/${DB_CONNECTION}/" src/SimplCommerce.WebHost/appsettings.json
+echo "SED done"
 
 rm -rf src/SimplCommerce.WebHost/Migrations/*
-
+echo "DOTNET BUILD CMD"
 dotnet restore && dotnet build
 
 cd src/SimplCommerce.WebHost \
